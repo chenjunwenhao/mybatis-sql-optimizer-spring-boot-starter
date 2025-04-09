decfd80e91c827b680f8ae9a90526d0afb955085
15046 <15046437592@139.com>
2025-04-09 20:28:24 +0800

    优化 注释

--- src/main/java/com/wuya/mybatis/optimizer/helper/PgParameterBinder.java
+++ src/main/java/com/wuya/mybatis/optimizer/helper/PgParameterBinder.java
@@ -1,5 +1,6 @@
 package com.wuya.mybatis.optimizer.helper;
 
+import com.wuya.mybatis.exception.SqlOptimizerException;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.reflection.MetaObject;
@@ -10,13 +11,21 @@ import java.sql.*;
 import java.util.*;
 import java.lang.reflect.Array;
 
+/**
+ * PostgreSQL参数绑定助手类
+ * 该类负责将MyBatis的参数对象转换为JDBC PreparedStatement所需的参数格式
+ * @author chenjunwen
+ * @date 2023-07-07
+ */
 public class PgParameterBinder {
 
     /**
-     * 安全绑定所有类型参数到PreparedStatement
-     * @param ps PreparedStatement对象
-     * @param boundSql MyBatis的BoundSql对象
-     * @param config MyBatis配置
+     * 绑定参数到PreparedStatement
+     * 
+     * @param ps PreparedStatement对象，用于执行SQL语句
+     * @param boundSql BoundSql对象，包含SQL语句和参数映射信息
+     * @param config Configuration对象，包含MyBatis配置信息
+     * @throws SQLException 当设置参数出错时抛出
      */
     public static void bindParameters(PreparedStatement ps,
                                       BoundSql boundSql,
@@ -36,7 +45,12 @@ public class PgParameterBinder {
     }
 
     /**
-     * 递归解析参数值（支持嵌套对象和集合）
+     * 解析参数值
+     * 
+     * @param metaObject MetaObject对象，用于访问参数对象的属性
+     * @param property 参数属性名
+     * @param config Configuration对象，包含MyBatis配置信息
+     * @return 解析后的参数值
      */
     private static Object resolveParameterValue(MetaObject metaObject,
                                                 String property,
@@ -56,7 +70,12 @@ public class PgParameterBinder {
     }
 
     /**
-     * 处理集合元素访问
+     * 解析集合元素
+     * 
+     * @param metaObject MetaObject对象，用于访问参数对象的属性
+     * @param property 参数属性名
+     * @param config Configuration对象，包含MyBatis配置信息
+     * @return 集合元素的值
      */
     private static Object resolveCollectionElement(MetaObject metaObject,
                                                    String property,
@@ -85,7 +104,12 @@ public class PgParameterBinder {
     }
 
     /**
-     * 处理嵌套属性访问
+     * 解析嵌套属性
+     * 
+     * @param metaObject MetaObject对象，用于访问参数对象的属性
+     * @param property 参数属性名
+     * @param config Configuration对象，包含MyBatis配置信息
+     * @return 嵌套属性的值
      */
     private static Object resolveNestedProperty(MetaObject metaObject,
                                                 String property,
@@ -101,6 +125,14 @@ public class PgParameterBinder {
         return current;
     }
 
+    /**
+     * 获取集合元素
+     * 
+     * @param collection 集合对象
+     * @param index 元素索引
+     * @return 集合元素的值
+     * @throws SqlOptimizerException 当集合类型不支持时抛出
+     */
     private static Object getCollectionElement(Object collection, int index) {
         if (collection instanceof List) {
             return ((List<?>) collection).get(index);
@@ -109,16 +141,30 @@ public class PgParameterBinder {
         } else if (collection instanceof Map) {
             return ((Map<?, ?>) collection).get(index);
         }
-        throw new IllegalArgumentException("不支持的集合类型: " + collection.getClass());
+        throw new SqlOptimizerException("不支持的集合类型: " + collection.getClass());
     }
 
+    /**
+     * 获取对象属性
+     * 
+     * @param obj 对象
+     * @param property 属性名
+     * @param config Configuration对象，包含MyBatis配置信息
+     * @return 属性值
+     */
     private static Object getObjectProperty(Object obj, String property, Configuration config) {
         MetaObject objMeta = config.newMetaObject(obj);
         return objMeta.getValue(property);
     }
 
     /**
-     * 安全设置参数值（包含Null处理）
+     * 设置PreparedStatement参数
+     * 
+     * @param ps PreparedStatement对象
+     * @param index 参数索引
+     * @param value 参数值
+     * @param jdbcType JDBC类型
+     * @throws SQLException 当设置参数出错时抛出
      */
     private static void setParameter(PreparedStatement ps,
                                      int index,
